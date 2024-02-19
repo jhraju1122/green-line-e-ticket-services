0f535e65501c1d7b835f76272f1f3a80646993d7
jhraju <jhraju890@gmail.com>
2024-02-19 12:00:03 +0600

    added seat color

---
+++ scripts.js/index.js
@@ -0,0 +1,23 @@
+
+// if I click the seat button then the button get Green color
+ 
+function seatColorA1(){
+    setSeatBackgroundColorById('A1');
+ }
+ 
+function seatColorA2(){
+    setSeatBackgroundColorById('A2');
+ }
+ 
+function seatColorA3(){
+    setSeatBackgroundColorById('A3');
+ }
+ 
+function seatColorA4(){
+    setSeatBackgroundColorById('A4');
+ }
+ 
+function seatColorA5(){
+    setSeatBackgroundColorById('A5');
+ }
+ 
---
+++ scripts.js/utility.js
@@ -0,0 +1,11 @@
+function setSeatBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-cyan-400');
+ } 
+
+//  function calculateSeatElement(textElementIt){
+//  const seatNumber = document.getElementById('textElementIt');
+//  const seatNumberCount = seatNumber.childElementCount;
+//  const p = document.createElement('p');
+//  p.innerText
+//  }
